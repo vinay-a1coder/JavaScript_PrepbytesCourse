bd1f4d72e551ccec34b0ee968f038f2f6141520b
vinay <vinay.a1.coder@gmail.com>
2022-02-04 13:51:57 -0800

    operators

---
+++ operators.js
@@ -0,0 +1,102 @@
+// Arithmetic operators
+
+// var add = 3+4;
+// console.log("Addition:",add);
+
+// var sub = 3-4;
+// console.log("Subtraction:",sub);
+
+// var mul = 3*4;
+// console.log("Multiplication:",mul);
+
+// var div = 3/4;
+// console.log("Division:",div);
+
+// var pow = 3**4;
+// console.log("Power:",pow);
+
+// var rem = 4%3;
+// console.log("Reminder:",rem);
+
+
+// Comparison operator
+
+// a = 2;
+// b = '2';
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(a==b);  // --> It compares only value.
+// console.log(a===b); // --> It compares value as well as data type.
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+// console.log(a!=b);  // --> It compares only value.
+// console.log(a!==b);  // --> It compares value as well as data type.
+
+
+// Increment and Decrement operator
+// var x = 5;
+// var y = 9;
+// var z = 3;
+// var w = 7;
+
+// console.log(x++);  // -->Post Increment
+
+// console.log(++y);  // -->Pre Increment 
+
+// console.log(z--);   //-->Post Decrement
+
+// console.log(--w);  //-->Pre Decrement
+
+
+
+// Logical operator
+
+// && --> true - true = true
+// || --> false - false = false
+// !  --> true->false; false->true
+
+// Logical and operator
+// var age=19;
+// var isboy = 1;
+// var isLicence = 1;
+// if(age>18 && isboy==1 && isLicence==1){
+//     console.log("Yes, you can drive!")
+// }
+// else{
+//     console.log("You can't drive.Sorry!")
+// }
+
+// Logical or operator
+// var age=17;
+// var isboy = 0;
+// var isLicence = 0;
+// if(age>18 || isboy==1 || isLicence==1){
+//     console.log("Yes, you can drive!")
+// }
+// else{
+//     console.log("You can't drive.Sorry!")
+// }
+
+// Logical not operator
+// var isboy=0;
+// console.log(!isboy);
+// var isLicence = 1;
+// console.log(!isLicence);
+// var age=17;
+// console.log(!age);
+
+
+// Conditional operator/Ternary operator(short-hand of if-else)
+
+// var age=17;
+// var isboy = 1;
+// var isLicence = 1;
+// console.log((age>18 || isboy==1 || isLicence==1) ? "Yes, you can drive!" : "You can't drive.Sorry!");
+
+
+// Comma operator
+// var a = 12;
+// var commaop = (a, ++a, a);
+// console.log(commaop);
--- variables_hoisting.js
+++ variables_hoisting.js
@@ -12,50 +12,50 @@
 // If we call function before creating it then it doesn't give error. It simply gives function result. 
 
 // Var can be redeclared and reassigned
-var a = 2;
-var a = 4;
-a = 6;
+// var a = 2;
+// var a = 4;
+// a = 6;
 
 // Let can be reassigned but cannot be redeclared
-let b = 3;
-b = 6;
+// let b = 3;
+// b = 6;
 
 // Const neither be redeclared nor be reassigned
-const c = 5; 
+// const c = 5; 
 
 
 
 // scope of variables
 
 // Global scope (var variable can be used outside a loop but we can't use it outside a function).
-if(2>=1){
-    var myName = 'vinay';
-}
-function func(name){
-    var myAge = 21;
-}
+// if(2>=1){
+//     var myName = 'vinay';
+// }
+// function func(name){
+//     var myAge = 21;
+// }
 // console.log(myName);
-console.log(myAge);  //--> It gives error because var  have function scope
+// console.log(myAge);  //--> It gives error because var  have function scope
 
 
 // Block scope(let variable neither be used outside a loop not from outside a function).
-if(2>=1){
-    let myName = 'vinay';
-}
-function func(name){
-    let myAge = 21;
-}
+// if(2>=1){
+//     let myName = 'vinay';
+// }
+// function func(name){
+//     let myAge = 21;
+// }
 // console.log(myAge); --> It gives error because let  function scope
 // console.log(myAge); -->It gives error because let  loop scope
 
 
 //(const variable can be used outside a loop but we can't use it outside a function).
-if(2>=1){
-    const myName = 'vinay';
-}
-function func(name){
-    const myAge = 21;
-}
+// if(2>=1){
+//     const myName = 'vinay';
+// }
+// function func(name){
+//     const myAge = 21;
+// }
 // console.log(myName);
 // console.log(myAge);  -->It gives error because const  have function scope
 
